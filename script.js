0d4bae44132a287464b6f969ea53acbf397e490d
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2025-03-07 21:53:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ navbarLinks.forEach(link => {
 closeMenuButton.addEventListener('click', () => openMenuButton.click());
 
 /* Swiper JS */
+let swiper = new Swiper('.slide-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  /* Responsive breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1080: {
+      slidesPerView: 3,
+    },
+  }
+});
